4a7464d316d4a06fbadf1aad5729eb43af2125d7
Marek Kobida <marek.kobida@gmail.com>
2025-03-24 13:18:21 +0100

    updated `NewUrl`

---
+++ NewUrl/index.test.ts
@@ -0,0 +1,18 @@
+/*
+ * Copyright 2025 Marek Kobida
+ * Last Updated: 24.03.2025
+ */
+
+import NewUrl from './index.js';
+import { expect, test } from 'bun:test';
+
+test('[1]', () => {
+  const newUrl = new NewUrl('https://kobida.sk:443/test.html');
+
+  expect(newUrl.host).toBe('https://kobida.sk:443');
+  expect(newUrl.path).toBe('/test.html');
+
+  newUrl.test('https://kobida.sk:443/{fileName?}');
+
+  expect(newUrl.parameters).toEqual({ fileName: 'test.html' });
+});
---
+++ NewUrl/index.ts
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2025 Marek Kobida
+ * Last Updated: 24.03.2025
+ */
+
+import Tokenizer from './Tokenizer.js';
+
+class NewUrl {
+  host = '';
+  readonly input: string;
+  parameters: { [key: string]: string } = {};
+  path = '';
+  searchParameters: { [key: string]: string } = {};
+  private testTokenizer?: Tokenizer;
+  private tokenizer: Tokenizer;
+
+  constructor(input: string) {
+    this.input = input;
+    this.tokenizer = new Tokenizer(this.input);
+
+    for (let i = 0; i < this.tokenizer.tokens.length; i++) {
+      const token = this.tokenizer.tokens[i]!;
+
+      // Sort: SCHEME, HOST, PORT, PATH, SEARCH_PARAMETER
+
+      if (token.type === 'SCHEME') {
+        this.host += token.value;
+      }
+
+      if (token.type === 'HOST') {
+        this.host += token.value;
+      }
+
+      if (token.type === 'PORT') {
+        this.host += `:${token.value}`;
+      }
+
+      if (token.type === 'PATH') {
+        this.path += `/${token.value}`;
+      }
+
+      if (token.type === 'SEARCH_PARAMETER') {
+        this.searchParameters[token.parameter[0]] = token.parameter[1];
+      }
+    }
+  }
+
+  // DOKONČIŤ
+  test(input: string): boolean {
+    this.testTokenizer = new Tokenizer(input);
+
+    for (let i = 0; i < this.testTokenizer.tokens.length; i++) {
+      const token1 = this.tokenizer.tokens[i];
+      const token2 = this.testTokenizer.tokens[i]!;
+
+      if (token2.type === 'HOST' || token2.type === 'PATH' || token2.type === 'PORT' || token2.type === 'SCHEME') {
+        if (!(token1 && token1.type === token2.type && token1.value === token2.value)) {
+          return false;
+        }
+      }
+
+      if (token2.type === 'PARAMETERIZED_PATH') {
+        //         ↓ je voliteľný?
+        if (token2.parameter[1]) {
+          if (token1) {
+            if (token1.type !== 'PATH') {
+              return false;
+            }
+
+            this.parameters[token2.parameter[0]] = token1.value;
+          }
+        } else {
+          if (!(token1 && token1.type === 'PATH')) {
+            return false;
+          }
+
+          this.parameters[token2.parameter[0]] = token1.value;
+        }
+      }
+    }
+
+    return true;
+  }
+}
+
+export default NewUrl;
