a9a0bdbb7add6179dcbc7a3fa0044f9e82b4df58
Marek Kobida <marek.kobida@gmail.com>
2025-10-12 23:26:49 +0200

    updated Router

--- Router/index.js
+++ Router/index.js
@@ -14,7 +14,7 @@ class Router {
         });
         return this;
     }
-    async getResponse(request) {
+    async getResponse(request, context) {
         const response = {
             body: {
                 $: new Uint8Array(),
@@ -70,7 +70,7 @@ class Router {
                 const newRouteUrl = `${request.url.host}${route.url}`;
                 if (request.url.test(newRouteUrl) &&
                     (isString(route.method) ? route.method === request.method : route.method.includes(request.method))) {
-                    await route.action(request, response);
+                    await route.action(request, response, context);
                     if (response.body.type === 'bytes' && response.body.$.length > 0) {
                         return response;
                     }
