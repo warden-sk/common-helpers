960600d03ecc89657ddc6da40ba8ce627d459a84
Marek Kobida <marek.kobida@gmail.com>
2026-01-06 02:17:12 +0100

    moved Where type to testWhere.ts file

--- effect/Repository/testWhere.ts
+++ effect/Repository/testWhere.ts
@@ -2,9 +2,11 @@
  * Copyright 2026 Marek Kobida
  */
 
+import type { Path, PathValue } from 'common-helpers/effect/Repository/getByPath.types.js';
+
 import { Option } from 'effect';
 
-import type { Row, Where } from './types.js';
+import type { Row } from './types.js';
 
 import isArray from '../../validation/isArray.js';
 import isBoolean from '../../validation/isBoolean.js';
@@ -13,7 +15,25 @@ import isObject from '../../validation/isObject.js';
 import isString from '../../validation/isString.js';
 import getByPath from './getByPath.js';
 
-function isComparable(input: unknown): input is boolean | number | string {
+type Comparable = boolean | number | string;
+
+type Where<T extends Row> = {
+  readonly $and?: readonly Where<T>[];
+  readonly $or?: readonly Where<T>[];
+} & {
+  readonly [TPath in Exclude<Path<T>, '$and' | '$or'>]?:
+    | PathValue<T, TPath>
+    | {
+        readonly $eq?: PathValue<T, TPath>; // EQUAL TO (=)
+        readonly $gt?: PathValue<T, TPath>; // GREATER THAN (>)
+        readonly $gte?: PathValue<T, TPath>; // GREATER THAN OR EQUAL TO (>=)
+        readonly $lt?: PathValue<T, TPath>; // LESS THAN (<)
+        readonly $lte?: PathValue<T, TPath>; // LESS THAN OR EQUAL TO (<=)
+        readonly $ne?: PathValue<T, TPath>; // NOT EQUAL TO (!=)
+      };
+};
+
+function isComparable(input: unknown): input is Comparable {
   return isBoolean(input) || isNumber(input) || isString(input);
 }
 
@@ -86,5 +106,7 @@ function testWhere<T extends Row>(row: T, where: Where<T>): boolean {
 
   return true;
 }
+
+export type { Where };
 
 export default testWhere;
--- effect/Repository/types.ts
+++ effect/Repository/types.ts
@@ -4,7 +4,7 @@
 
 import type { Effect, Option } from 'effect';
 
-import type { Path, PathValue } from './getByPath.types.js';
+import type { Where } from './testWhere.js';
 
 type Filter<T extends Row> = {
   readonly where: Where<T>;
@@ -24,20 +24,4 @@ type Row = {
   readonly _id: string;
 };
 
-type Where<T extends Row> = {
-  readonly $and?: readonly Where<T>[];
-  readonly $or?: readonly Where<T>[];
-} & {
-  readonly [TPath in Exclude<Path<T>, '$and' | '$or'>]?:
-    | PathValue<T, TPath>
-    | {
-        readonly $eq?: PathValue<T, TPath>; // EQUAL TO (=)
-        readonly $gt?: PathValue<T, TPath>; // GREATER THAN (>)
-        readonly $gte?: PathValue<T, TPath>; // GREATER THAN OR EQUAL TO (>=)
-        readonly $lt?: PathValue<T, TPath>; // LESS THAN (<)
-        readonly $lte?: PathValue<T, TPath>; // LESS THAN OR EQUAL TO (<=)
-        readonly $ne?: PathValue<T, TPath>; // NOT EQUAL TO (!=)
-      };
-};
-
-export type { Filter, Repository, Row, Where };
+export type { Filter, Repository, Row };
