67486e2e75154e1d24d82fef79940c882bfb82c5
Marek Kobida <marek.kobida@gmail.com>
2026-02-24 14:50:59 +0100

    fixed

--- getByPath/index.ts
+++ getByPath/index.ts
@@ -5,28 +5,12 @@
 import type { Path, PathValue } from './types.js';
 
 // ✅
-function getByPath<
-  T extends {
-    [key: string]: any;
-  },
-  TPath extends Path<T>,
->(input: T, path: TPath): PathValue<T, TPath> {
+function getByPath<T extends Record<string, any>, TPath extends Path<T>>(input: T, path: TPath): PathValue<T, TPath> {
   const keys = path.split('.');
 
   return keys.reduce(($, key) => {
     return $[key];
   }, input) as PathValue<T, TPath>;
 }
-
-function setByPath<
-  T extends {
-    [key: string]: any;
-  },
-  TPath extends Path<T>,
->(input: T, path: TPath, value: PathValue<T, TPath>): T {
-  return input;
-}
-
-export { setByPath };
 
 export default getByPath;
