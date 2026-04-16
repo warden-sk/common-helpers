3caaf70081ef4a2d3f6b48b756fb022de8a59660
Marek Kobida <marek.kobida@gmail.com>
2026-04-16 12:12:55 +0200

    fixed

--- validation/isArray.ts
+++ validation/isArray.ts
@@ -1,8 +1,8 @@
 /*
- * Copyright 2025 Marek Kobida
+ * Copyright 2026 Marek Kobida
  */
 
-function isArray(input: unknown): input is unknown[] {
+function isArray(input: unknown): input is readonly unknown[] {
   return Object.prototype.toString.call(input) === '[object Array]';
 }
 
